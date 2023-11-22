ea18eb92689453c9e3c0a8e8b394e7c0577bd55a
harshitmourya <85952883+harshitmourya@users.noreply.github.com>
2023-11-22 16:34:52 +0530

    update wicket over

--- controllers/twoMatchData.js
+++ controllers/twoMatchData.js
@@ -6,6 +6,8 @@ const SecondInning = require("../models/SecondInnings");
 
 var message = ""
 var isBattingTeam = '';
+var isFound = " ";
+var teamOverCount = '';
 
 async function twoMatch(req, res) {
     var battingTeamID = req.body.battingTeamID;
@@ -20,40 +22,89 @@ async function twoMatch(req, res) {
     console.log("BattingTeam :-", battingTeamID);
     console.log("BowlingTeam :-", bowlingTeamID);
 
+
     async function isTeam() {
         try {
-            
-            isBattingTeam = await FirstInning.findOne().sort({createdAt:-1}).exec();
-            const isSecondBattingTeam = await SecondInning.findOne().sort({createdAt:-1 }).exec();
 
-            console.log("FirstInning :-",isBattingTeam);
+            isBattingTeam = await FirstInning.findOne().sort({ createdAt: -1 }).exec();
 
-            if(isBattingTeam == null){
-                console.log("error :-",error.message);
-                throw error
+            if (!isBattingTeam) {
+
+                // checking the team overCount is end or not
+                const teamOverLimit = 50;
+                const teamWicketLimit = 11;
+
+                // checking teamOverCount is equal or greater than teamOverCount
+                const isteamOver = isBattingTeam.teamOverCount >= teamOverLimit;
+                console.log("overs are end", isteamOver)
+
+                // checking teamWicketCount is equal or greater than teamWicketlimit
+                const isteamWicket = isBattingTeam.WicketCount >= teamWicketLimit;
+                console.log("all batsman are out", isteamWicket);
+                console.log("FirstInning",isBattingTeam)
+
+                if(teamOverLimit || teamWicketLimit){
+                    console.log('First Inning is Over');
+                    return true;
+
+                }else {
+                    console.log("first Inning is running");
+                    return false;
+                }
+
+
+            } else {
+
+                const isSecondBattingTeam = await SecondInning.findOne().sort({ createdAt: -1 }).exec();
+                
+                //checking the team overCount is end or not
+                const teamOverLimit = 50;
+                const teamWicketLimit = 11;
+
+                // checking teamOverCount is equal or greater than teamOverCount
+                const isteamOver = isSecondBattingTeam.teamOverCount >= teamOverLimit;
+                console.log("overs are end", isteamOver)
+
+                // checking teamWicketCount is equal or greater than teamWicketlimit
+                const isteamWicket = isSecondBattingTeam.WicketCount >= teamWicketLimit;
+                console.log("all batsman are out", isteamWicket);
+
+                console.log("SecondInning:-",isSecondBattingTeam);
+
+                if(teamOverLimit || teamWicketLimit){
+                    console.log('Second Inning is Over');
+                    message = "Second Inning is Over";
+                    return true;
+
+                }else {
+                    console.log("Second Inning is running");
+                    message ="Second Inning is running";
+                    return false;
+                }
+
+               
             }
-            
-                //const isSecondBattingTeam = await SecondInning.findOne({BattingTeamID:battingTeamID})
-            console.log("SecondInning :-",isSecondBattingTeam);
-            }
-        
+        }
+
         catch (error) {
             console.log(error.message, " BattingTeam and BowlingTeam not found");
         }
-        res.status(200).send({message:isBattingTeam})
+        res.status(200).send({ message: isBattingTeam })
     }
 
     try {
 
         const allTeamData = await twoTeamDetail.save();
         console.log(allTeamData);
-        
-        isTeam();
-        
 
-        res.status(200).json({ message: "Two match data saved successfully",
-    
-         isBattingTeam});
+        isTeam();
+
+
+        res.status(200).json({
+            message: "Two match data saved successfully",
+
+            isBattingTeam
+        });
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Error saving two match data" });
@@ -69,10 +120,9 @@ module.exports = twoMatch;
 
 
 // function generateMatchID(isBattingTeam,isBowlingTeam) {
-        //     // Create a unique match ID based on team IDs and a timestamp
-        //     const timestamp = Date.now(); // Get the current timestamp
-        //     const matchID = `${battingTeamID}-${bowlingTeamID}-${timestamp}-${uuid.v4()}`;
-        //     console.log(matchID);
-        //     return matchID;
-        // }
-        
+//     // Create a unique match ID based on team IDs and a timestamp
+//     const timestamp = Date.now(); // Get the current timestamp
+//     const matchID = `${battingTeamID}-${bowlingTeamID}-${timestamp}-${uuid.v4()}`;
+//     console.log(matchID);
+//     return matchID;
+// }
