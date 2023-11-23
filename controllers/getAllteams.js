cc85a71a5d3c45c22603a677cdb8789860b92580
harshitmourya <85952883+harshitmourya@users.noreply.github.com>
2023-11-23 17:28:57 +0530

    update getall

--- controllers/getAllteams.js
+++ controllers/getAllteams.js
@@ -6,7 +6,7 @@ async function getAllteam(req,res){
     try {
         const teamAll = await teamDetail.find();
         console.log(teamAll);
-        res.status(200).json({teamAll});  
+        res.status(200).json(teamAll);  
     } catch (error) {
         console.log("error",error.message)
     } 
