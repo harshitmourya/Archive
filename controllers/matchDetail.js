129f2c96871a696f5dc8564e1d2da4bacc52eceb
harshitmourya <85952883+harshitmourya@users.noreply.github.com>
2023-11-23 16:00:08 +0530

    update change

--- controllers/matchDetail.js
+++ controllers/matchDetail.js
@@ -1,4 +1,5 @@
 const matchDetail = require("../models/matchDetail");
+const MatchDetail = require("../models/matchDetail");
 const TeamDetail = require("../models/teamDetail");
 
 async function saveMatchDetails(req, res) {
@@ -19,60 +20,61 @@ async function saveMatchDetails(req, res) {
 
         async function isExistTeam() {
             try {
-                const isteam1 = await TeamDetail.findOne({ _id: req.body.team1_id });
-                const isteam2 = await TeamDetail.findOne({ _id: req.body.team2_id });
+                const isteam1 = await TeamDetail.findOne({ _id:team1_id });
+                const isteam2 = await TeamDetail.findOne({ _id:team2_id });
 
                 console.log("team1ID :-", isteam1);
                 console.log("team2ID :-", isteam2);
 
-              
+                if(!isteam1 || isteam2){
+                    return res.status(400).json({message:"One or both teams not found"})
+                }
+
             } catch (error) {
-                console.log("Error finding teams:", error.message);
-                return res.status(500).json({ message: "Internal Server Error" });
+                console.log("Team Not found", error.message);
             }
+
+            const ExistingMatch = await MatchDetail.findOne({
+                team1_id: req.body.team1_id,
+                team2_id: req.body.team2_id
+            });
+
+            if (ExistingMatch) {
+                ExistingMatch.team1_id = req.body.team1_id;
+                ExistingMatch.team2_id = req.body.team2_id;
+                ExistingMatch.team1Name = req.body.team1Name;
+                ExistingMatch.team2Name = req.body.team2Name;
+                ExistingMatch.TosswinningTeamId = req.body.TosswinningTeamId;
+                ExistingMatch.totalOver = req.body.totalOver;
+                ExistingMatch.choose = req.body.choose;
+
+                await ExistingMatch.save();
+
+            } else {
+                const secondExist = await new MatchDetail({
+                    team1_id: req.body.team1_id,
+                    team1Name: req.body.team1Name,
+                    team2_id: req.body.team2_id,
+                    team2Name: req.body.team2Name,
+                    totalOver: req.body.totalOver,
+                    TosswinningTeamId: req.body.TosswinningTeamId,
+                    choose: req.body.choose
+                });
+
+                await secondExist.save();
+            }
+
+            res.status(200).json({ message: "MatchDetail data saved successfully" });
         }
 
         await isExistTeam();
-
-        const ExistingMatch = await matchDetail.findOne({
-            team1_id: req.body.team1_id,
-            team2_id: req.body.team2_id
-        });
-
-        if (ExistingMatch) {
-            ExistingMatch.team1_id = req.body.team1_id;
-            ExistingMatch.team2_id = req.body.team2_id;
-            ExistingMatch.team1Name = req.body.team1Name;
-            ExistingMatch.team2Name = req.body.team2Name;
-            ExistingMatch.TosswinningTeamId = req.body.TosswinningTeamId;
-            ExistingMatch.totalOver = req.body.totalOver;
-            ExistingMatch.choose = req.body.choose;
-
-            await ExistingMatch.save();
-        } else {
-            const newMatch = new matchDetail({
-                team1_id: req.body.team1_id,
-                team1Name: req.body.team1Name,
-                team2_id: req.body.team2_id,
-                team2Name: req.body.team2Name,
-                totalOver: req.body.totalOver,
-                TosswinningTeamId: req.body.TosswinningTeamId,
-                choose: req.body.choose
-            });
-
-            await newMatch.save();
-        }
-
-        res.status(200).json({ message: "MatchDetail data saved successfully" });
-
     } catch (error) {
-        console.log("Error in saveMatchDetails:", error.message);
-        res.status(500).json({ message: "Internal Server Error" });
+        console.log("error :-",error.message);
+        res.status(500).json({ message: "Team not found" });
     }
 }
 
 module.exports = saveMatchDetails;
-
 
  // const matchdetail = new MatchDetail(req.body);
     // var msg = "", win, loose, team1, team2;
