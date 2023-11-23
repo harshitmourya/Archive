ce8478bd0587fe49d5c8f8bbc2951efdb7199a8d
harshitmourya <85952883+harshitmourya@users.noreply.github.com>
2023-11-23 18:31:26 +0530

    update error console

--- controllers/getAllteam.js
+++ controllers/getAllteam.js
@@ -2,20 +2,14 @@ const teamDetail = require("../models/teamDetail");
 
 
 const getAllteam = async (req, res) => {
-    const teamAll = await teamDetail.find();
-    console.log(teamAll);
-    res.status(200).json(teamAll);
+    try {
+        const teamAll = await teamDetail.find();
+        console.log(teamAll);
+        res.status(200).json(teamAll);   
+    } catch (error) {
+        console.log("error fetching team data :-",error.message);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 }
 
-
-// async function getAllteam(req,res){
-//     try {
-//         const teamAll = await teamDetail.find();
-//         console.log(teamAll);
-//         res.status(200).json(teamAll);  
-//     } catch (error) {
-//         console.log("error",error.message)
-//     } 
-// }
-// getAllteam()
 module.exports = getAllteam
