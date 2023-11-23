8129af3c487cb6e3e125b72eaf1938e1cabf6dea
harshitmourya <85952883+harshitmourya@users.noreply.github.com>
2023-11-23 17:18:19 +0530

    get try catch

--- controllers/getAllteams.js
+++ controllers/getAllteams.js
@@ -3,8 +3,12 @@ const teamDetail = require("../models/teamDetail");
 
 
 async function getAllteam(req,res){
-    const teamAll = await teamDetail.find();
-    console.log(teamAll);
-    res.status(200).json({teamAll});
+    try {
+        const teamAll = await teamDetail.find();
+        console.log(teamAll);
+        res.status(200).json({teamAll});  
+    } catch (error) {
+        console.log("error",error.message)
+    } 
 }
 module.exports = getAllteam
