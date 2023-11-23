a3c5659dfa7eb28b45c8829501a0864afdf600de
harshitmourya <85952883+harshitmourya@users.noreply.github.com>
2023-11-23 18:01:40 +0530

    get allteam

--- controllers/getAllteam.js
+++ controllers/getAllteam.js
@@ -1,13 +1,21 @@
 const teamDetail = require("../models/teamDetail");
 
-async function getAllteam(req,res){
-    try {
-        const teamAll = await teamDetail.find();
-        console.log(teamAll);
-        res.status(200).json(teamAll);  
-    } catch (error) {
-        console.log("error",error.message)
-    } 
+
+const getAllteam = async (req, res) => {
+    const teamAll = await Player.find();
+    console.log(teamAll);
+    res.status(200).json(getAllteam);
 }
-getAllteam()
+
+
+// async function getAllteam(req,res){
+//     try {
+//         const teamAll = await teamDetail.find();
+//         console.log(teamAll);
+//         res.status(200).json(teamAll);  
+//     } catch (error) {
+//         console.log("error",error.message)
+//     } 
+// }
+// getAllteam()
 module.exports = getAllteam
