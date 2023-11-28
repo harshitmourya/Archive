e308b0a18ad9e2c0f4d0eeef6b0cbae4a892abbb
harshitmourya <85952883+harshitmourya@users.noreply.github.com>
2023-11-28 16:34:11 +0530

    onground change

--- controllers/playerOnGroundDetail.js
+++ controllers/playerOnGroundDetail.js
@@ -2,20 +2,18 @@ const Player = require("../models/playerOnGroundDetail");
 const Team = require("../models/teamDetail");
 
 const savePlayerOnGroundDetails = async (req, res) => {
-    //console.log("Request Body",req.body);
-    // for (const file of files)
     var isPlayerFound = false;
     var isTeamFound = true;
-    var data = "";
     var msg = "";
-    // var message = "";
     var teamId = "", firstname = "", teamName = "", playerID = "";
+
     console.log("Request body: ", req.body);
     const obj = req.body;
-    for(const element of obj){
+
+    for (const element of obj) {
         const player = new Player(element);
         console.log("Given Object: ", player);
-        console.log("Object: ", element)
+        console.log("Object: ", element);
         firstname = element.playerFullName;
         console.log("First Name: ", firstname);
         playerID = element.playerID;
@@ -24,67 +22,68 @@ const savePlayerOnGroundDetails = async (req, res) => {
         console.log("Team ID: ", teamId);
         teamName = element.teamName;
         console.log("Team Name: ", teamName);
-        const teamDataName = await Team.findOne({team: teamName});
-        const teamDataId = await Team.findOne({_id: teamId});
-        console.log("New object Id", player._id)
+
+        const teamDataName = await Team.findOne({ team: teamName });
+        const teamDataId = await Team.findOne({ _id: teamId });
+
+        console.log("New object Id", player._id);
         console.log("Team Data Name: ", teamDataName);
         console.log("Player Team ID: ", teamDataId);
-        if (teamDataName == null) {
-            console.log("Team Not Found")
+
+        if (teamDataName == null || teamDataId == null) {
+            console.log("Team Not Found");
             isTeamFound = false;
             break;
-        }else if(teamDataId == null) {
-            console.log("Team Id Not Founc")
-            isTeamFound = false
-            break
-        }else {
-            isTeamFound = true
-            await player.save()
-            msg = "Player with Team Added Successfully."
+        } else {
+            isTeamFound = true;
+
+            // Uncomment the following block if you want to check for existing players with the same contact
+            // try {
+            //     const data = await Player.findOne({ playerContact: contact });
+            //     console.log("Data: ", data);
+            //     if (data) {
+            //         console.log("Player Already Exist");
+            //         isPlayerFound = true;
+            //         break;
+            //     } else {
+            //         await player.save();
+            //         console.log("Player Added Successfully");
+            //         msg = "Player with Team Added Successfully.";
+            //     }
+            // } catch (error) {
+            //     console.log("Error: ", error);
+            //     return res.status(500).send({
+            //         error: "Internal server error",
+            //     });
+            // }
+            // End of the commented block
+
+            // Include this part if you're not checking for existing players
+            await player.save();
+            console.log("Player Added Successfully");
+            msg = "Player with Team Added Successfully.";
         }
-        // try {
-        //     data = await Player.findOne({playerContact: contact});
-        //     console.log("Data: ", data)
-        // }catch(error){
-        //     console.log("Error: ",error);
-        //     data = ""
-        // }
-        // if (data){
-        //     console.log("Player Already Exist");
-        //     isPlayerFound = true;
-        //     break;
-        // }else {
-        //     await player.save()
-        //     console.log("Inside Else Condition.");
-        // }  
+
         console.log("Inside For Each at the end");
     }
+
     console.log("Outside For Each");
-    // if (isPlayerFound || (!isTeamFound)) {
+
     if (!isTeamFound) {
-        // var msg = ""
-        // if (isPlayerFound) {
-        //     msg = "Contact is already exist.";
-        // }
-        // else if (!isTeamFound){
-        //     msg = "Team not found.";
-        // }
         console.log("Message: ", msg);
         return res.status(400).send({
             user: firstname,
-            // contact: contact,
             teamName: teamName,
-            message: msg
-        })
+            message: msg,
+        });
     } else {
         console.log("Success");
-        console.log("Request Body: ", req.body)
-        return res.status(201).send(req.body)
+        console.log("Request Body: ", req.body);
+        return res.status(201).send(req.body);
     }
 };
 
 module.exports = savePlayerOnGroundDetails;
-
 
 
 
