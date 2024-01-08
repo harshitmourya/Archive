2ab6dab587a8b49597dad93b4f04f9fe27aa6d90
harshitmourya <85952883+harshitmourya@users.noreply.github.com>
2024-01-08 14:50:13 +0530

    update params

--- controllers/getAllPlayer.js
+++ controllers/getAllPlayer.js
@@ -1,21 +1,22 @@
 const Player = require("../models/playerDetail");
 
 const getAllPlayers = async (req, res) => {
-    const {Players} = req.params;
+    // const {Players} = req.params;
 
     try {
-        const allPlayers = await Player.find(Players);
-        if (!allPlayers) {
+        const allPlayers = await Player.find({userID:req.params.userID});
+        console.log( "userID -", userID)
+        if (!allPlayers.length) {
             res.status(404).json({ message: 'User not found' });
             return
-        }
+        }else{
         res.status(200).json(allPlayers);
         console.log(allPlayers);
-
+        }
     } catch (error) {
         console.log(error.message)
     }
 
-}
+}   
 
 module.exports = getAllPlayers;
