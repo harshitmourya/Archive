395f3dc125105706ee87012ed2b49835b64887bd
harshitmourya <85952883+harshitmourya@users.noreply.github.com>
2024-01-09 13:06:39 +0530

    change in schema

--- models/saveMatchBTW2teams.js
+++ models/saveMatchBTW2teams.js
@@ -11,6 +11,16 @@ const twoMatchDataSchema = new mongoose.Schema({
         type : mongoose.Schema.Types.ObjectId,
         required: true,
         ref:"FirstInning"
+    },
+    userID:{
+        type:mongoose.Schema.Types.ObjectId,
+        require:true,
+        ref:"registrationDetail"
+    },
+    username:{
+        type:mongoose.Schema.Types.String,
+        required:true,
+        ref:"registrationDetail"
     }
  
 });
